5853c7f96863027adb21b472aa3a88c3e03a9ee4
Ujjwal <ujjwalgupta220505@gmail.com>
2026-01-03 19:05:47 +0530

    Resolve the Cors issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify,CORS
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import joblib
 from sklearn.preprocessing import MultiLabelBinarizer
